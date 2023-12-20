d4648a9e743d00c20b7b5bd6bf62ca6e578fc281
CaptSiro <honzahorak433@icloud.com>
2023-12-21 00:16:27 +0100

    Automatic file deletion for add, select, delete. Added --slot=<s> arg for add

--- spheal.py
+++ spheal.py
@@ -25,27 +25,56 @@ def todo(message: str):
 def cmd_help(program: str):
     print(f"Usage: {program} <command>")
     print(f"\n\t[, help]\n\t\tShows this help")
-    print(f"\n\tadd path\n\t\tAdds path but it is not selected")
+    print(f"\n\tadd path [, --slot=<slot_name>]\n\t\tAdds path but it is not selected")
     print(f"\n\tdelete id\n\t\tDeletes path with given entry. Use command `ls` to see ids for all saved paths")
     print(f"\n\tls [, pattern]\n\t\tLists all saved paths. Use optional parameter pattern to search in saved paths")
     print(f"\n\tset setting value\n\t\tSets value of specified setting\n\t\t\tWriteable settings: slots")
     print(f"\n\tselect slot id\n\t\tWrites path of given id to specified slot. Slot is number and it is appended to the path of setting 'slots'")
 
 
-def cmd_add(args):
-    if len(args) < 1:
+class Args:
+    def __init__(self, common, named):
+        self.named = named
+        self.common = common
+
+    @staticmethod
+    def parse(args) -> "Args":
+        common = []
+        named = {}
+
+        for arg in args:
+            a = str(arg)
+            if a.startswith("--"):
+                name, value = a.split("=", 1)
+                named[str(name).replace("--", '')] = value
+                continue
+
+            common.append(a)
+
+        return Args(common, named)
+
+
+
+def cmd_add(args: Args):
+    if len(args.common) < 1:
         print("Missing path argument")
         exit(1)
 
     conf = conf_load()
 
     for path_id, path, slot in conf["paths"]:
-        if path == args[0]:
+        if path == args.common[0]:
             print(f"Path `{path}` is already saved under id `{path_id}`")
             return
 
-    conf["paths"].append([conf["index"], args[0], "NULL"])
+    path_id = conf["index"]
+    conf["paths"].append([path_id, args.common[0], "NULL"])
     conf["index"] += 1
+
+    if "slot" in args.named:
+        select_slot(conf, args.named["slot"], path_id)
+        return
+
     conf_save(conf)
 
 
@@ -55,17 +84,21 @@ def index_of(array, predicate):
             return i
 
     return None
-def cmd_delete(args):
-    if len(args) < 1:
+def cmd_delete(args: Args):
+    if len(args.common) < 1:
         print("Missing path id argument")
         exit(1)
 
     conf = conf_load()
 
-    index = index_of(conf["paths"], lambda p: str(p[0]) == str(args[0]))
+    index = index_of(conf["paths"], lambda p: str(p[0]) == str(args.common[0]))
     if index is None:
-        print(f"Could not find path with id `{args[0]}`")
+        print(f"Could not find path with id `{args.common[0]}`")
         return
+
+    slot = conf["paths"][index][2]
+    if slot != "NULL":
+        os.unlink(conf["slots"] + slot)
 
     conf["paths"].pop(index)
     conf_save(conf)
@@ -83,27 +116,27 @@ def matches_pattern(pattern, subject):
                 return True
 
     return False
-def cmd_ls(args):
+def cmd_ls(args: Args):
     conf = conf_load()
     if len(conf["paths"]) == 0:
         print("No paths saved...")
         return
 
     print("[id]\tslot\tpath")
-    argc = len(args)
+    argc = len(args.common)
     for path_id, path, slot in conf["paths"]:
-        if argc > 0 and not matches_pattern(args[0], path):
+        if argc > 0 and not matches_pattern(args.common[0], path):
             continue
 
         print(f"[{path_id}]\t{'' if slot == 'NULL' else slot}\t{path}")
 
 
-def cmd_set(args):
-    if len(args) < 2:
+def cmd_set(args: Args):
+    if len(args.common) < 2:
         print("Not enough arguments. Expected <setting> and <value>")
         exit(1)
 
-    setting, value = args
+    setting, value = args.common
     if setting in READONLY_SETTINGS:
         print(f"Can not modify read only setting `{setting}`")
         exit(1)
@@ -115,14 +148,7 @@ def cmd_set(args):
     print(f"{setting}: {value}")
 
 
-def cmd_select(args):
-    if len(args) < 2:
-        print("Not enough arguments. Expected <slot> and <id>")
-        return
-
-    conf = conf_load()
-    slot, path_id = args
-
+def select_slot(conf, slot, path_id):
     i = index_of(conf["paths"], lambda p: str(p[0]) == str(path_id))
     if i is None:
         print(f"Could not find path with id `{path_id}`")
@@ -132,6 +158,7 @@ def cmd_select(args):
 
     old = index_of(conf["paths"], lambda p: str(p[2]) == str(slot))
     if old is not None:
+        os.unlink(conf["slots"] + conf["paths"][old][2])
         conf["paths"][old][2] = "NULL"
 
     file_name = conf["slots"] + str(slot)
@@ -142,6 +169,14 @@ def cmd_select(args):
     path[2] = slot
 
     conf_save(conf)
+def cmd_select(args: Args):
+    if len(args.common) < 2:
+        print("Not enough arguments. Expected <slot> and <id>")
+        return
+
+    conf = conf_load()
+    slot, path_id = args.common
+    select_slot(conf, slot, path_id)
 
 
 def main():
@@ -151,8 +186,9 @@ def main():
         cmd_help(program)
         return
 
+    _, command, *arg_list = sys.argv
+    args = Args.parse(arg_list)
 
-    _, command, *args = sys.argv
     if command == "help":
         cmd_help(program)
     elif command == "add":
